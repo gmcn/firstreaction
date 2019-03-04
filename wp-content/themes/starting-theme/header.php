532cec4d53b42d2cb15551d823e5e63d58c3ab15
Web Dev <webdev@Webs-MBP.lan>
2019-03-04 17:18:43 +0000

    header to change based on meeting

--- wp-content/themes/starting-theme/header.php
+++ wp-content/themes/starting-theme/header.php
@@ -17,58 +17,113 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
-<link type="text/plain" rel="robots" href="<?php echo get_template_directory_uri(); ?>/humans.txt" />
-<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/robots.txt" />
+<link type="text/plain" rel="humans" href="<?php echo get_template_directory_uri(); ?>/humans.txt" />
+<link type="text/plain" rel="robots" href="<?php echo get_template_directory_uri(); ?>/robots.txt" />
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'starting-theme' ); ?></a>
+	<?php
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	$description = get_bloginfo( 'description', 'display' );
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+	 ?>
+	<header>
+		<div class="container-fluid">
+			<div class="row no-gutter">
+				<div class="col-md-4 site-branding">
+					<h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo( 'name' ); ?>"></a></h1>
+				</div><!-- /.col-md-4 .site-branding -->
+				<div class="col-md-4 shop">
+					<a href="https://facebook.com" target="_blank">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/fb_header.svg" alt="Follow us First Reaction on Facebook">
+					</a>
+					<a href="https://twitter.com" target="_blank">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/twitter_header.svg" alt="Follow us First Reaction on Twitter">
+					</a>
 
-		<!-- Static navbar -->
-	      <nav class="navbar navbar-default">
-	        <div class="container-fluid">
-	          <div class="navbar-header">
-	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	              <span class="sr-only">Toggle navigation</span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	            </button>
-	          </div>
-	            <?php wp_nav_menu( array(
-								'theme_location' => 'menu-1',
-								'menu_id' => 'navbar',
-								'container_id' => 'navbar',
-								'container_class' => 'navbar-collapse collapse',
-								'menu_class' => 'navbar-collapse',
-								'items_wrap' => '<ul id="" class="nav navbar-nav navbar-right">%3$s</ul>' ) );
-								?>
-	            <ul class="nav navbar-nav navbar-right">
-	              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-	              <li><a href="../navbar-static-top/">Static top</a></li>
-	              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-	            </ul>
-	        </div><!--/.container-fluid -->
-	      </nav><!-- #site-navigation -->
+					<nav class="navbar yamm" role="navigation">
+				     <ul class="nav navbar-nav navbar-shop">
+				       <li class="dropdown">
+				         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop</a>
+								 <ul class="dropdown-menu">
+                <li>
+                  <!-- Content container to add padding -->
+                  <div class="yamm-content">
+                    <div class="row">
+                      <ul class="col-sm-6 list-unstyled">
+                        <li>
+                          <p><strong>Section Title</strong></p>
+                        </li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                        <li>List Item</li>
+                      </ul>
+                      <ul class="col-sm-6 list-unstyled">
+                        <li>
+                          <p><strong>Links Title</strong></p>
+                        </li>
+                        <li><a href="#"> Link Item </a></li>
+                        <li><a href="#"> Link Item </a></li>
+                        <li><a href="#"> Link Item </a></li>
+                        <li><a href="#"> Link Item </a></li>
+                        <li><a href="#"> Link Item </a></li>
+                        <li><a href="#"> Link Item </a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+                </li>
+              </ul>
+				       </li>
+				     </ul>
+					</nav>
+
+					<?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
+					    $count = WC()->cart->cart_contents_count;
+					    ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+					    if ( $count > 0 ) {
+					        ?>
+					        <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+					        <?php
+					    }
+					        ?></a>
+
+					<?php } ?>
+
+				</div><!-- /.col-md-4 -->
+				<div class="col-md-4">
+					<!-- Static navbar -->
+				      <nav class="navbar navbar-default">
+				        <div class="container-fluid">
+				          <div class="navbar-header">
+				            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				              <span class="sr-only">Toggle navigation</span>
+				              <span class="icon-bar"></span>
+				              <span class="icon-bar"></span>
+				              <span class="icon-bar"></span>
+				            </button>
+				          </div>
+				            <?php wp_nav_menu( array(
+											'theme_location' => 'menu-1',
+											'menu_id' => 'navbar',
+											'container_id' => 'navbar',
+											'container_class' => 'navbar-collapse collapse',
+											'menu_class' => 'navbar-collapse',
+											'items_wrap' => '<ul id="" class="nav navbar-nav navbar-right">%3$s</ul>' ) );
+											?>
+				        </div><!--/.container-fluid -->
+				      </nav><!-- #site-navigation -->
+				</div><!-- /.col-md-4 -->
+			</div>
+		</div><!-- /.container-fluid -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
