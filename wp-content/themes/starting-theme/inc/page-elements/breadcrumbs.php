d59f8c780736d63c6d3dcbc55155f9c1be893f2b
Web Dev <webdev@Webs-MBP.lan>
2019-03-11 12:07:22 +0000

    breadcrumbs site element

---
+++ wp-content/themes/starting-theme/inc/page-elements/breadcrumbs.php
@@ -0,0 +1,11 @@
+<div class="container-fluid breadcrumbs">
+  <div class="row">
+    <div class="col-md-12">
+      <?php
+      if ( function_exists('yoast_breadcrumb') ) {
+        yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+      }
+      ?>
+    </div>
+  </div>
+</div>
